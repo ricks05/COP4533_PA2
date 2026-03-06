64c28c10c98ac95a0a368cd99a85efb940a43718
Rick Silva <hbsilva2005@gmail.com>
2026-03-06 11:55:38 -0500

    create .gitignore and refactor project files

---
+++ Cache.cpp
@@ -0,0 +1,5 @@
+//
+// Created by hbsil on 3/5/2026.
+//
+
+#include "Cache.h"
---
+++ Cache.h
@@ -0,0 +1,16 @@
+//
+// Created by hbsil on 3/5/2026.
+//
+
+#ifndef PA2_CACHE_H
+#define PA2_CACHE_H
+
+
+
+class Cache {
+
+};
+
+
+
+#endif //PA2_CACHE_H
